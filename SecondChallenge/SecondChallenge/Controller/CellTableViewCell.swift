d6dc2052fdd9dc7ce0b0f7eac31bfd51a5b73896
dallin smuin <dallinsmuin@gmail.com>
2019-05-16 07:57:59 -0600

    added textfield and label on storyborad

---
+++ SecondChallenge/SecondChallenge/Controller/CellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellTableViewCell.swift
+//  SecondChallenge
+//
+//  Created by Dallin Smuin on 5/15/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SecondChallenge/SecondChallenge/Model/Entry.swift
@@ -0,0 +1,17 @@
+//
+//  Entry.swift
+//  SecondChallenge
+//
+//  Created by Dallin Smuin on 5/15/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import Foundation
+
+// The object of the entry values.
+struct Entry {
+    let nameOfFood: String
+    let calories: String
+    let dayConsumed: String
+    let rating: String
+}
